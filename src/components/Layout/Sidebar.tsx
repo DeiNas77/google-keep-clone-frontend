5c29694047c2894a535bed44808a11298f57fd27
DeiNas77 <deinas.44Remastered@gmail.com>
2026-02-19 18:04:26 -0500

    GKCF-01 | WIP: change duration in animation and change link in renderLinks and finish TODO

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -3,18 +3,16 @@ import { SidebarLink } from "../common/SidebarLink";
 import { SidebarProps } from "../types/NavbarProps";
 
 const iconsRender = [
-	{ href: "/notes", text: "Notas", icon: Lightbulb },
+	{ href: "/", text: "Notas", icon: Lightbulb },
 	{ href: "/archive", text: "Archivar", icon: Archive },
 	{ href: "/trash", text: "Papelera", icon: Trash },
 ];
-
-//TODO: el w del aside debe estar en w-15 al estar cerrado. Y el hover debe rodearlo con un con un full al icon a la hora de cerrarse
 
 export const Sidebar = ({ isOpen }: SidebarProps) => {
 	return (
 		<aside
 			className={`
-				border-r transition-all duration-300
+				 transition-all duration-200 border-r
 				${isOpen ? "w-64 overflow-auto" : "w-17 pl-1.5 overflow-hidden"}
 			`}>
 			<nav className="flex flex-col py-3 gap-1">
