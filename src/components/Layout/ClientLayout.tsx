616b2e912f69f2e2ad043c7c1297d86a5dd10d3d
DeiNas77 <deinas.44Remastered@gmail.com>
2026-02-23 21:46:15 -0500

    GKCF-02 | chore: reemplazing tags in ClientLayout and Navbar

--- src/components/Layout/ClientLayout.tsx
+++ src/components/Layout/ClientLayout.tsx
@@ -9,13 +9,13 @@ export const ClientLayout = ({ children }: { children: React.ReactNode }) => {
 	const handleOpen = () => setIsOpen((prev) => !prev);
 
 	return (
-		<div className="flex flex-col h-dvh">
+		<section className="flex flex-col h-dvh">
 			<Navbar handleOpen={handleOpen} />
 
 			<div className="flex flex-1 overflow-hidden">
 				<Sidebar isOpen={isOpen} />
 				<main className="flex-1 overflow-auto p-4"> {children}</main>
 			</div>
-		</div>
+		</section>
 	);
 };
--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -13,19 +13,19 @@ import { NavbarProps } from "../types/NavbarProps";
 export const Navbar = ({ handleOpen }: NavbarProps) => {
 	return (
 		<nav className="flex justify-between w-full py-2 px-3.5 border-b items-center">
-			<div className="flex items-center">
+			<section className="flex items-center">
 				<IconButton icon={Menu} onClick={handleOpen} />
 				{/* //Aquí ira el link describiendo el contexto */}
 				<h1 className="text-2xl pl-2">Keep</h1>
-			</div>
-			<div className="relative w-100">
+			</section>
+			<form role="search" className="relative w-100">
 				<Search className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400" />
 				<input
 					type="text"
 					placeholder="Buscar"
 					className="w-full pl-9 pr-3 py-2 bg-white border rounded-md focus:outline-none focus:ring-2 "
 				/>
-			</div>
+			</form>
 			<div className="flex gap-2">
 				<IconButton icon={RotateCcw} />
 				<IconButton icon={LayoutGrid} />
