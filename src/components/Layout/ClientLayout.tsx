974e41cc35f10527a7699a6ddf06858411f9eb70
DeiNas77 <deinas.44remastered@gmail.com>
2026-08-26 23:21:12 -0500

    GKCF-05 | feat: split Sidebar into Desktop and Mobile drawer

--- src/components/Layout/ClientLayout.tsx
+++ src/components/Layout/ClientLayout.tsx
@@ -1,29 +1,30 @@
 "use client";
 import { AppProvider } from "../context/AppContext";
-import { Sidebar } from "./Sidebar";
+import { SidebarDesktop, SidebarMobile } from "./Sidebar";
 import { Navbar } from "./Navbar";
 import { useAppContext } from "../context/AppContext";
 
 const ClientLayoutInner = ({ children }: { children: React.ReactNode }) => {
-	const { isGrid, toggleGrid, isSidebarOpen, toggleSidebar } = useAppContext();
+  const { isGrid, toggleGrid, isSidebarOpen, toggleSidebar } = useAppContext();
 
-	return (
-		<section className="flex flex-col h-dvh">
-			<Navbar
-				handleOpen={toggleSidebar}
-				handleGrid={isGrid}
-				setHandleGrid={toggleGrid}
-			/>
-			<div className="flex flex-1 overflow-hidden">
-				<Sidebar isOpen={isSidebarOpen} />
-				<main className="flex-1 overflow-auto p-4">{children}</main>
-			</div>
-		</section>
-	);
+  return (
+    <section className="flex flex-col h-dvh">
+      <Navbar
+        handleOpen={toggleSidebar}
+        handleGrid={isGrid}
+        setHandleGrid={toggleGrid}
+      />
+      <div className="flex flex-1 overflow-hidden">
+        <SidebarDesktop isOpen={isSidebarOpen} />
+        <SidebarMobile isOpen={isSidebarOpen} onClose={toggleSidebar} />
+        <main className="flex-1 overflow-auto p-4">{children}</main>
+      </div>
+    </section>
+  );
 };
 
 export const ClientLayout = ({ children }: { children: React.ReactNode }) => (
-	<AppProvider>
-		<ClientLayoutInner>{children}</ClientLayoutInner>
-	</AppProvider>
+  <AppProvider>
+    <ClientLayoutInner>{children}</ClientLayoutInner>
+  </AppProvider>
 );
